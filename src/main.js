b7465886904ed36fc56486bcd4dc99b5977d0797
Miguel De Jesus <8982063-mdesarrollador23@users.noreply.gitlab.com>
2021-09-04 03:26:15 -0400

    modulo de producto casi creado

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueSweetalert2 from 'vue-sweetalert2';
+import '@/style.scss'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = "http://localhost:4000/api"
+axios.defaults.headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzI1ODE3NzE5NzZhMTVhMGE4ZmYyZiIsImVtYWlsIjoibWRlc2Fycm9sbGFkb3IyM0BnbWFpbC5jb20iLCJpYXQiOjE2MzA2ODkzMDUsImV4cCI6MTYzMDc3NTcwNX0.j94Ft2LqAY7ocotRO4sPAcJogf_LpVTQQ22lF0ZxyvI'
+}
+
 Vue.use(VueAxios, axios)
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
--- src/views/home/Product.vue
+++ src/views/home/Product.vue
@@ -13,6 +13,7 @@
         loading-text="Por favor... espere un momento."
         class="elevation-1"
         dense
+        :items-per-page="50"
         :footer-props="{
           itemsPerPageAllText: 'Todo',
           itemsPerPageText: 'Filas por página',
@@ -57,13 +58,28 @@
                   <v-toolbar-title v-text="formTitle"></v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-toolbar-items>
-                    <v-btn dark text @click="save"> Save </v-btn>
+                    <v-btn
+                      :disabled="$v.$invalid"
+                      :loading="btnLoading"
+                      dark
+                      text
+                      @click="save"
+                    >
+                      Guardar
+                    </v-btn>
                   </v-toolbar-items>
                 </v-toolbar>
                 <v-card-text>
                   <v-container>
                     <v-row>
-                      <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                      <v-col
+                        cols="12"
+                        sm="2"
+                        md="2"
+                        lg="2"
+                        xl="2"
+                        v-if="editedIndex < 1"
+                      >
                         <label class="font-weight-bold" for=""
                           >Generar Codigo
                         </label>
@@ -76,7 +92,14 @@
                           >
                         </div>
                       </v-col>
-                      <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                      <v-col
+                        cols="12"
+                        sm="2"
+                        md="2"
+                        lg="2"
+                        xl="2"
+                        v-if="editedIndex < 1"
+                      >
                         <label class="font-weight-bold" for=""
                           >Codigo Barra
                           <span class="error--text">*</span></label
@@ -85,10 +108,13 @@
                           :disabled="inputBarCode"
                           solo
                           required
-                          v-model.trim="editedItem.barCode"
-                          hide-details
+                          v-model="editedItem.barcode"
+                          :hide-details="hideDetails(barcodeErrors.length)"
+                          :error-messages="barcodeErrors"
+                          @input="$v.editedItem.barcode.$touch()"
+                          @blur="$v.editedItem.barcode.$touch()"
                           label="#*****"
-                          type="text"
+                          type="number"
                         ></v-text-field>
                       </v-col>
                       <v-col cols="12" sm="8" md="8" lg="8" xl="8">
@@ -98,8 +124,11 @@
                         <v-text-field
                           solo
                           required
-                          v-model.trim="editedItem.description"
-                          hide-details
+                          v-model="editedItem.description"
+                          :hide-details="hideDetails(descriptionErrors.length)"
+                          :error-messages="descriptionErrors"
+                          @input="$v.editedItem.description.$touch()"
+                          @blur="$v.editedItem.description.$touch()"
                           label="Descripción"
                           type="text"
                         ></v-text-field>
@@ -111,8 +140,13 @@
                         <v-text-field
                           solo
                           required
-                          v-model.trim="editedItem.referenceLarge"
-                          hide-details
+                          v-model="editedItem.referencelarge"
+                          :hide-details="
+                            hideDetails(referencelargeErrors.length)
+                          "
+                          :error-messages="referencelargeErrors"
+                          @input="$v.editedItem.referencelarge.$touch()"
+                          @blur="$v.editedItem.referencelarge.$touch()"
                           label="Referencia Larga"
                           type="text"
                         ></v-text-field>
@@ -124,8 +158,13 @@
                         <v-text-field
                           solo
                           required
-                          v-model.trim="editedItem.referenceSmall"
-                          hide-details
+                          v-model="editedItem.referencesmall"
+                          :hide-details="
+                            hideDetails(referencesmallErrors.length)
+                          "
+                          :error-messages="referencesmallErrors"
+                          @input="$v.editedItem.referencesmall.$touch()"
+                          @blur="$v.editedItem.referencesmall.$touch()"
                           label="Referencia Corta"
                           type="text"
                         ></v-text-field>
@@ -135,10 +174,13 @@
                         <v-text-field
                           solo
                           required
-                          v-model.trim="editedItem.stock"
-                          hide-details
+                          v-model="editedItem.stock"
+                          :hide-details="hideDetails(stockErrors.length)"
+                          :error-messages="stockErrors"
+                          @input="$v.editedItem.stock.$touch()"
+                          @blur="$v.editedItem.stock.$touch()"
                           label="Stock"
-                          type="text"
+                          type="number"
                         ></v-text-field>
                       </v-col>
                     </v-row>
@@ -164,36 +206,25 @@
                   >
                   <v-spacer></v-spacer>
                   <v-toolbar-items>
-                    <v-btn dark text @click="deleteItemConfirm"> Save </v-btn>
+                    <v-btn
+                      dark
+                      text
+                      :loading="btnDLoading"
+                      @click="deleteItemConfirm"
+                    >
+                      Save
+                    </v-btn>
                   </v-toolbar-items>
                 </v-toolbar>
-                <!-- <v-card-title class="text-h5"
-                  >Seguro que desea deshabilitar este registro?</v-card-title
-                >
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete"
-                    >Cancelar</v-btn
-                  >
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                    >OK</v-btn
-                  >
-                  <v-spacer></v-spacer>
-                </v-card-actions> -->
               </v-card>
             </v-dialog>
           </v-toolbar>
         </template>
-        <template v-slot:item.options="{ item }">
+
+        <template v-slot:[`item.options`]="{ item }">
           <v-tooltip top>
             <template v-slot:activator="{ on, attrs }">
-              <v-icon
-                v-bind="attrs"
-                v-on="on"
-                small
-                class="mr-2"
-                @click="editItem(item)"
-              >
+              <v-icon v-bind="attrs" v-on="on" small class="mr-2">
                 mdi-chart-areaspline
               </v-icon>
             </template>
@@ -226,60 +257,92 @@
           <v-btn color="primary" @click="getProducts"> Actualizar </v-btn>
         </template>
         <template v-slot:no-results> Lo que esta buscando no aparece </template>
-        <template v-slot:no-results> Lo que esta buscando no aparece </template>
       </v-data-table>
     </v-card>
     <svg id="barcode"></svg>
   </v-container>
 </template>
 <script>
-import products from "@/data/products.json";
-// import JsBarcode from "jsbarcode";
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  maxLength,
+  minLength,
+  decimal,
+  helpers,
+} from "vuelidate/lib/validators";
+const alpha = helpers.regex("alpha", /^[a-zA-Z-0-9- ]*$/);
+
 export default {
+  mixins: [validationMixin],
+  validations: {
+    editedItem: {
+      barcode: {
+        required,
+        decimal,
+      },
+      description: {
+        required,
+        alpha,
+        maxLength: maxLength(100),
+        minLength: minLength(1),
+      },
+      referencelarge: {
+        alpha,
+        maxLength: maxLength(50),
+        minLength: minLength(1),
+      },
+      referencesmall: {
+        alpha,
+        maxLength: maxLength(25),
+        minLength: minLength(1),
+      },
+      stock: {
+        decimal,
+      },
+    },
+  },
   name: "Dashboard-Product",
   data: () => ({
     search: null,
     tableLoading: false,
+    btnLoading: false,
+    btnDLoading: false,
     headers: [
       {
         text: "Codigo Barra",
         align: "start",
-        value: "barCode",
+        value: "barcode",
       },
       { text: "Descripción", sortable: true, value: "description" },
-      { text: "Referencia", value: "referenceLarge" },
-      { text: "Ref Corta", value: "referenceSmall" },
+      { text: "Referencia", value: "referencelarge" },
+      { text: "Ref Corta", value: "referencesmall" },
       { text: "Compra", value: "buy" },
       { text: "Venta", value: "sale" },
       { text: "Stock", value: "stock" },
       { text: "Existencia", value: "existence" },
+      { text: "Fecha", value: "createdDate" },
       { text: "Opciones", align: "end", value: "options", sortable: false },
     ],
     inputBarCode: false,
-    products: null,
+    products: [],
     dialog: false,
     dialogDelete: false,
     dialogSaveLoading: false,
     editedIndex: -1,
     editedItem: {
-      barCode: null,
+      barcode: null,
       description: null,
-      referenceLarge: null,
-      referenceSmall: null,
-      buy: null,
-      sale: null,
-      stock: null,
-      existence: null,
+      referencelarge: null,
+      referencesmall: null,
+      stock: 0,
     },
     defaultItem: {
-      barCode: null,
+      barcode: null,
       description: null,
-      referenceLarge: null,
-      referenceSmall: null,
-      buy: null,
-      sale: null,
-      stock: null,
-      existence: null,
+      referencelarge: null,
+      referencesmall: null,
+      stock: 0,
     },
   }),
   watch: {
@@ -290,14 +353,6 @@ export default {
       val || this.closeDelete();
     },
   },
-  mounted() {
-    // JsBarcode("#barcode")
-    //   .EAN13("123456789012", {
-    //     height: 50,
-    //     fontSize: 16,
-    //   })
-    //   .render();
-  },
   created() {
     this.getProducts();
   },
@@ -305,10 +360,11 @@ export default {
   methods: {
     generateBarCode() {
       this.inputBarCode = true;
-      this.editedItem.barCode =
+      this.editedItem.barcode =
         Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
     },
     editItem(item) {
+      this.inputBarCode = true;
       this.editedIndex = this.products.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
@@ -321,8 +377,7 @@ export default {
     },
 
     deleteItemConfirm() {
-      this.products.splice(this.editedIndex, 1);
-      this.closeDelete();
+      this.deleteProduct(this.editedItem);
     },
 
     close() {
@@ -343,18 +398,146 @@ export default {
     },
 
     save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.products[this.editedIndex], this.editedItem);
-      } else {
-        this.products.push(this.editedItem);
-      }
-      this.close();
+      if (this.editedIndex > -1) this.updateProduct(this.editedItem);
+      else this.createProduct(this.editedItem);
     },
-    getProducts() {
-      this.products = products;
+    async getProducts() {
+      this.products = [];
+      this.tableLoading = true;
+      await this.axios
+        .get("/product")
+        .then((res) => {
+          this.products = res.data;
+          this.tableLoading = false;
+        })
+        .catch((err) => {
+          this.$swal({
+            icon: "error",
+            title: "Oops...",
+            text: err.response.data.msg,
+          });
+        });
+    },
+    async createProduct(data) {
+      this.btnLoading = true;
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        await this.axios
+          .post("/product", data)
+          .then((res) => {
+            this.products.push(res.data.item);
+            this.$v.$reset();
+            this.btnLoading = false;
+            this.$swal("Buen Trabajo!", "El producto fue creado!", "success");
+            this.close();
+          })
+          .catch((err) => {
+            this.$swal({
+              icon: "error",
+              title: "Oops...",
+              text: err.response.data.msg,
+            });
+          });
+      }
+    },
+    async updateProduct(data) {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        await this.axios
+          .put(`/product/${data._id}`, data)
+          .then((res) => {
+            Object.assign(this.products[this.editedIndex], res.data.item);
+            this.close();
+          })
+          .catch((err) => {
+            this.$swal({
+              icon: "error",
+              title: "Oops...",
+              text: err.response.data.msg,
+            });
+          });
+      }
+    },
+    async deleteProduct(data) {
+      this.tableLoading = true;
+      this.btnDLoading = true;
+      await this.axios
+        .delete(`/product/${data._id}`)
+        .then(() => {
+          this.products.splice(this.editedIndex, 1);
+          this.closeDelete();
+          this.tableLoading = false;
+          this.btnDLoading = false;
+          this.$swal("Buen Trabajo!", "El producto fue borrado!", "success");
+        })
+        .catch((err) => {
+          this.$swal({
+            icon: "error",
+            title: "Oops...",
+            text: err.response.data.msg,
+          });
+        });
+    },
+    hideDetails(val) {
+      if (val <= 0) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
   computed: {
+    stockErrors() {
+      const errors = [];
+      if (!this.$v.editedItem.stock.$dirty) return errors;
+      !this.$v.editedItem.stock.decimal &&
+        errors.push("El codigo de barra solo permite numeros.");
+      return errors;
+    },
+    referencesmallErrors() {
+      const errors = [];
+      if (!this.$v.editedItem.referencesmall.$dirty) return errors;
+      !this.$v.editedItem.referencesmall.maxLength &&
+        errors.push("La descripcion debe tener 100 caracteres como máximo.");
+      !this.$v.editedItem.referencesmall.minLength &&
+        errors.push("La descripcion debe tener 1 caracteres como minimo.");
+      !this.$v.editedItem.referencesmall.alpha &&
+        errors.push("Solo es permitido a-z-A-Z-0-9.");
+      return errors;
+    },
+    referencelargeErrors() {
+      const errors = [];
+      if (!this.$v.editedItem.referencelarge.$dirty) return errors;
+      !this.$v.editedItem.referencelarge.maxLength &&
+        errors.push("La descripcion debe tener 100 caracteres como máximo.");
+      !this.$v.editedItem.referencelarge.minLength &&
+        errors.push("La descripcion debe tener 1 caracteres como minimo.");
+      !this.$v.editedItem.referencelarge.alpha &&
+        errors.push("Solo es permitido a-z-A-Z-0-9.");
+      return errors;
+    },
+    descriptionErrors() {
+      const errors = [];
+      if (!this.$v.editedItem.description.$dirty) return errors;
+      !this.$v.editedItem.description.required &&
+        errors.push("La descripcion es requerido.");
+      !this.$v.editedItem.description.maxLength &&
+        errors.push("La descripcion debe tener 100 caracteres como máximo.");
+      !this.$v.editedItem.description.minLength &&
+        errors.push("La descripcion debe tener 1 caracteres como minimo.");
+      !this.$v.editedItem.description.alpha &&
+        errors.push("Solo es permitido a-z-A-Z-0-9.");
+      return errors;
+    },
+    barcodeErrors() {
+      const errors = [];
+      if (!this.$v.editedItem.barcode.$dirty) return errors;
+      !this.$v.editedItem.barcode.required &&
+        errors.push("El codigo de barra es requerido.");
+      !this.$v.editedItem.barcode.decimal &&
+        errors.push("El codigo de barra solo permite numeros.");
+      return errors;
+    },
     formTitle() {
       return this.editedIndex === -1 ? "Nuevo Producto" : "Editar Producto";
     },
